9273ccf0ef0c095f767a463b225c80f36bee173b
champton15 <calibhampton@gmail.com>
2026-04-29 15:28:15 -0400

    Update geoapify.js

--- services/apis/geoapify.js
+++ services/apis/geoapify.js
@@ -1,13 +1,13 @@
-const API_KEY = b071fca2216e4d3bb2cbea11fad11019;
+const API_KEY = "b071fca2216e4d3bb2cbea11fad11019";
 
-export async function geocode(city) {
+async function geocode(city) {
     const res = await fetch(
         `https://api.geoapify.com/v1/geocode/search?text=${city}&apiKey=${API_KEY}`
     );
     return await res.json();
 }
 
-export async function getPlaces(lat, lon, category) {
+async function getPlaces(lat, lon, category) {
     const res = await fetch(
         `https://api.geoapify.com/v2/places?categories=${category}&filter=circle:${lon},${lat},5000&limit=6&apiKey=${API_KEY}`
     );
